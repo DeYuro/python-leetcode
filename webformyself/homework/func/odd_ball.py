7ce71f0438cd98b1c0265383b630452d9ad8a9aa
Denis Shashkov <dshashkov@beget.ru>
2022-06-13 23:15:01 +0300

    odd_ball

---
+++ webformyself/homework/func/odd_ball.py
@@ -0,0 +1,17 @@
+def odd_ball(arr):
+    try:
+        arr.index(arr.index('odd'))
+    except ValueError:
+        return False
+
+    return True
+
+
+list1 = [
+    ["even", 4, "even", 7, "even", 55, "even", 6, "even", 10, "odd", 3, "even"],
+    ["even", 4, "even", 7, "even", 55, "even", 6, "even", 9, "odd", 3, "even"],
+    ["even", 10, "odd", 2, "even"],
+]
+
+for i in list1:
+    print(odd_ball(i))
