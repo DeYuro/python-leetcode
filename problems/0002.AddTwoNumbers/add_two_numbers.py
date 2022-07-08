d0fcbd441b9055018708db1debfe31cd0f72d119
Denis Shashkov <deyurosh@gmail.com>
2022-07-08 21:45:49 +0300

    add struct and testcase

--- problems/0002.AddTwoNumbers/add_two_numbers.py
+++ problems/0002.AddTwoNumbers/add_two_numbers.py
@@ -1,9 +1,4 @@
 from typing import Optional
-
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         pass
